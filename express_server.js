b22627126972e915772d88e3026eaf9a03169b4f
Michael Xiang <yorkmike4@gmail.com>
2020-12-08 23:10:41 +0000

    login/logout

--- express_server.js
+++ express_server.js
@@ -2,8 +2,12 @@ const express = require ("express");
 const app = express();
 const PORT = 3000;
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser")
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(cookieParser());
+
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -23,22 +27,29 @@ app.get("/", (req, res) => {
 })
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase }
+  const templateVars = { username: req.cookies["username"], urls: urlDatabase }
   res.render("urls_index", templateVars);
 })
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("urls_new",templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = { username: req.cookies["username"], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
 
 app.post("/urls/:shortURL/delete", (req,res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect('/urls')
+})
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username").redirect('/urls')
 })
 
 app.post("/urls/:id", (req,res) => {
@@ -51,6 +62,10 @@ app.post("/urls", (req, res) => {
   urlDatabase[newKey] = req.body.longURL;
   res.redirect(`/urls/${newKey}`)
 });
+
+app.post("/login",(req,res) => {
+  res.cookie("username",req.body.username).redirect("/urls")
+})
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
