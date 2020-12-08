e577873d52ad487757468de13e18e96505a3fcc7
Michael Xiang <yorkmike4@gmail.com>
2020-12-08 07:34:58 +0000

    new submit ok, random generate

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require ("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -20,16 +22,24 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
-
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
+function generateRandomString() {
+    return Math.random().toString(36).substring(2,8);
+}
