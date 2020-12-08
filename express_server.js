fc3dffddc9abaad3dd2eb1e78bee9e50b4afdcc0
Michael Xiang <yorkmike4@gmail.com>
2020-12-08 00:06:21 +0000

    short url

--- express_server.js
+++ express_server.js
@@ -7,21 +7,28 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+app.listen(PORT,() => {
+  console.log(`Example app listening on port ${PORT}!`);
+})
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 })
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase }
   res.render("urls_index", templateVars);
 })
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render("urls_show", templateVars);
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.listen(PORT,() => {
-  console.log(`Example app listening on port ${PORT}!`);
-})
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
