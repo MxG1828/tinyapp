bd64b14016b0517702a0b8048a31ed9891438664
Michael Xiang <yorkmike4@gmail.com>
2020-12-11 08:43:12 +0000

    fixed some bugs, add readme, screenshot

--- express_server.js
+++ express_server.js
@@ -93,13 +93,18 @@ app.get("/u/:shortURL", (req, res) => {
   }
   urlDatabase[req.params.shortURL].visits += 1;
   const longURL = urlDatabase[req.params.shortURL].longURL;
-  res.redirect(longURL);
+  return res.redirect(longURL);
 });
 
 app.post("/urls", (req, res) => {
   const newKey = generateRandomString();
+  //if user's input does not have http://, add one
+  let long = req.body.longURL
+  if (req.body.longURL.indexOf("://") === -1) {
+    long = "http://" + long
+  }
   urlDatabase[newKey] = {
-    longURL: req.body.longURL,
+    longURL: long,
     userId: req.session.user_id,
     date: new Date(Date.now()).toLocaleDateString(),
     visits: 0,
@@ -115,15 +120,15 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${newKey}`);
 });
 
-app.post("/urls/:id", (req, res) => {
-  const templateVars = { shortURL: req.params.id, error, user: users[req.session.user_id] };
+app.post("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, error, user: users[req.session.user_id] };
   //if not login
   if (!req.session.user_id) {
     templateVars.error = "noLogin";
     return res.render("error", templateVars);
   }
   //if user dont have access
-  if (Object.keys(urlForUser(req.session.user_id, urlDatabase)).indexOf(req.params.id) === -1) {
+  if (Object.keys(urlForUser(req.session.user_id, urlDatabase)).indexOf(req.params.shortURL) === -1) {
     templateVars.error = "noAccess";
     return res.render("error", templateVars);
   }
@@ -131,7 +136,12 @@ app.post("/urls/:id", (req, res) => {
   if (!req.body.longURL) {
     return res.redirect("/urls");
   }
-  urlDatabase[req.params.id].longURL = req.body.longURL;
+  //if user's input does not have http://, add one
+  let longURL = req.body.longURL;
+  if (req.body.longURL.indexOf("://") === -1) {
+    longURL = "http://" + longURL
+  }
+  urlDatabase[req.params.shortURL].longURL = longURL;
   res.redirect("/urls");
 });
 
